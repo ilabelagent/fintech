234b8698899c5b558fed7b300d4a5189d39b1579
iman-git <smoothjmark@gmail.com>
2025-11-24 04:05:01 +0000

    kj
    jhk

--- valifi/drizzle.config.ts
+++ valifi/drizzle.config.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
   schema: "./shared/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
-} satisfies Config;
+});
